7de7eab2e6aba1d66f8d2c95d5a0a0c3427ed166
mohammad <M@>
2023-12-26 21:52:11 +0330

    beterpics

--- src/components/coLogoContainer/index.tsx
+++ src/components/coLogoContainer/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import CenterFlex from '../wraper/centerFlex'
 import CenterFlexCol from '../wraper/centerFlexCol'
-import pic1 from '../../../public/icon/icon.png'
-import pic2 from '../../../public/icon/public.png'
+import pic1 from '../../../public/icon/metavers.png'
+import pic2 from '../../../public/icon/block.png'
 import pic3 from '../../../public/icon/vote.png'
 import Image from 'next/image'
 
@@ -12,15 +12,15 @@ function CologoContainer() {
         <CenterFlex>
             <CenterFlexCol>
                 <Image className='lg:scale-100 md:scale-90 scale-75' src={pic1} alt='' width={40}/>
-                <div className='md:mt-3 lg:mt-1 text-[9px] md:text-xs'>عادلانه</div>
+                <div className='md:mt-3 lg:mt-1 text-[9px] md:text-xs'>metavers</div>
             </CenterFlexCol>
             <CenterFlexCol>
                 <Image className='lg:scale-100 md:scale-90 scale-75' src={pic2} alt='' width={40}/>
-                <div className='md:mt-3 lg:mt-1 text-[9px] md:text-xs'>لبه فناوری</div>
+                <div className='md:mt-3 lg:mt-1 text-[9px] md:text-xs'>blockchain</div>
             </CenterFlexCol>
             <CenterFlexCol>
                 <Image className='lg:scale-100 md:scale-90 scale-75' src={pic3} alt='' width={40}/>
-                <div className='md:mt-3 lg:mt-1 text-[9px] md:text-xs'>شفاف</div>
+                <div className='md:mt-3 lg:mt-1 text-[9px] md:text-xs'>electral</div>
             </CenterFlexCol>
         </CenterFlex>
     </div>
