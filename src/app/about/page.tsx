6405cf0b8eae261cb57afaad52b85ce35f61f9aa
mohammad <M@>
2023-12-27 18:26:48 +0330

    aboutresizde

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,7 +12,7 @@ function About() {
         <div className={`${myFont.className} mt-8 text-[72px]`}>
             <MajazLogo/>
         </div>
-        <div className='w-[80vw]  text-[18px] md:w-[70vw] lg:w-[68vw] ms-[10vw] md:ms-[15vw] lg:ms-[18vw] mt-[10vh] text-right'>
+        <div dir='rtl' className='w-[86vw] tracking-tighter text-justify text:[8px] leading-6 lg:text-[18px] md:w-[70vw] lg:w-[68vw] me-[7vw] md:me-[15vw] lg:me-[18vw] mt-8 lg:mt-[10vh]'>
              مجاز یک پروژه خصوصی نیست! مجاز تلاشی برای همگانی کردن منافع و قدرت تصمیم گیری در دنیای آیندمونه. نظر ما اینه دنیا با گذشت زمان و پیشرفت تکنولوژی عادلانه تر میشه و تکنولوژی های جدید مفاهیم فرهنگ و اعتماد و عدالت رو دچار تغییر میکنند. اینجا ما تلاش میکنیم جهت این تغییرات مثبت باشه 
              
         </div>
