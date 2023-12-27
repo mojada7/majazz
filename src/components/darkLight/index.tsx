c53efa7cb88950fca066227b560be4756858657c
mohammad <M@>
2023-12-27 15:04:48 +0330

    whitesun

--- src/components/darkLight/index.tsx
+++ src/components/darkLight/index.tsx
@@ -1,8 +1,5 @@
 'use client'
-import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
-import light from '../../../public/icon/light.png'
-import dark from '../../../public/icon/dark.png'
 import { useTheme } from 'next-themes'
 import Sun from '@heroicons/react/24/solid/SunIcon'
 import Moon from '@heroicons/react/24/solid/MoonIcon'
@@ -17,7 +14,7 @@ function DarkLight() {
   
   const LightBt = ()=> {
     return(
-      <Sun className="h-8 w-8 text-sky-200"/>
+      <Sun className="h-8 w-8 text-white"/>
     )
   }
   const DarkBt = ()=> {
