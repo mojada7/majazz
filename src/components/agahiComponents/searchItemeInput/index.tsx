e8c29310453a679cae284d0841ceaf292e3156ac
mohammad <M@>
2023-12-28 12:38:01 +0330

    searchbarfontresize

--- src/components/agahiComponents/searchItemeInput/index.tsx
+++ src/components/agahiComponents/searchItemeInput/index.tsx
@@ -5,7 +5,7 @@ function SearchItemeInput() {
   return (
     <div className='w-full px-1 rounded-xl border-[2px] dark:bg-stone-950 border-gray-300 flex items-center md:py-1'>
 
-        <input dir='rtl' className='w-full px-2 rounded-xl dark:bg-stone-950 border-none outline-none py-1' placeholder='بگرد دنبال چیزی که میخوای ...'/>
+        <input dir='rtl' className='w-full px-2 rounded-xl dark:bg-stone-950 border-none outline-none py-1 placeholder:text-[11px]' placeholder='بگرد دنبال چیزی که میخوای ...'/>
         <div className={style.flip}>
             <SeatchIcon className='h-6 w-6 text-gray-400' />
         </div>
