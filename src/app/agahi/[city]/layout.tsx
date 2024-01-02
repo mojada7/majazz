1f5e18b3c88d18e60dc8f276aa7768db298f2fe2
mohammad <M@>
2024-01-02 19:04:18 +0330

    logo

--- src/app/agahi/[city]/layout.tsx
+++ src/app/agahi/[city]/layout.tsx
@@ -6,6 +6,8 @@ import Add from '@heroicons/react/24/outline/PlusCircleIcon'
 import Chat from '@heroicons/react/24/outline/ChatBubbleBottomCenterTextIcon'
 import Ads from '@heroicons/react/24/outline/NewspaperIcon'
 import Category from '@heroicons/react/24/outline/InboxStackIcon'
+import Image from "next/image";
+import logo from '../../../../public/icon/logo.png'
 
 export default function CityLayout (
     {children} : {children : ReactNode}
@@ -15,19 +17,21 @@ export default function CityLayout (
             {children}
             <div className="w-[100vw] bg-gray-50 dark:bg-stone-900 h-[12vh] fixed bottom-0 left-0 flex justify-between">
                 <BottomMenuCardWraper text="ثبت آگهی" >
-                    <Add className="w-8 h-8"></Add>
+                    <Add className="w-6 h-7 md:w-8 md:h-8"></Add>
                 </BottomMenuCardWraper>
                 <BottomMenuCardWraper text='دسته‌بندی' >
-                    <Category className="w-8 h-8"></Category>
+                    <Category className="w-6 h-7 md:w-8 md:h-8"></Category>
                 </BottomMenuCardWraper>
                 <BottomMenuCardWraper text='آگهی‌ها' >
-                    <Ads className="w-8 h-8 "></Ads>
+                    <div className=" w-6 h-7 md:w-8 md:h-8 relative">
+                    <Image src={logo} fill alt="آگهی" />
+                    </div>
                 </BottomMenuCardWraper>
                 <BottomMenuCardWraper text='پیام‌ها' >
-                    <Chat className="w-8 h-8"></Chat>
+                    <Chat className="w-6 h-7 md:w-8 md:h-8"></Chat>
                 </BottomMenuCardWraper>
                 <BottomMenuCardWraper text='حساب کاربری' >
-                    <Account className="w-8 h-8">
+                    <Account className="w-6 h-7 md:w-8 md:h-8">
                     </Account>
                 </BottomMenuCardWraper>
             </div>
