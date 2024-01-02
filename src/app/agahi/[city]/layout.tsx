e4ebc360489f6726537435947b67d0ec81e8358a
mohammad <M@>
2024-01-02 17:23:12 +0330

    menu

--- src/app/agahi/[city]/layout.tsx
+++ src/app/agahi/[city]/layout.tsx
@@ -1,6 +1,11 @@
 
 import BottomMenuCardWraper from "@/components/agahiComponents/bottomMenuCardWraper";
 import { ReactNode } from "react";
+import Account from '@heroicons/react/24/outline/UserCircleIcon'
+import Add from '@heroicons/react/24/outline/PlusCircleIcon'
+import Chat from '@heroicons/react/24/outline/ChatBubbleBottomCenterTextIcon'
+import Ads from '@heroicons/react/24/outline/NewspaperIcon'
+import Category from '@heroicons/react/24/outline/InboxStackIcon'
 
 export default function CityLayout (
     {children} : {children : ReactNode}
@@ -10,19 +15,20 @@ export default function CityLayout (
             {children}
             <div className="w-[100vw] bg-gray-50 dark:bg-stone-900 h-[12vh] fixed bottom-0 left-0 flex justify-between">
                 <BottomMenuCardWraper text="ثبت آگهی" >
-                    <div className="w-12 h-12 bg-slate-600"></div>
+                    <Add className="w-8 h-8"></Add>
                 </BottomMenuCardWraper>
-                <BottomMenuCardWraper text="ثبت آگهی" >
-                    <div className="w-12 h-12 bg-slate-600"></div>
+                <BottomMenuCardWraper text='دسته‌بندی' >
+                    <Category className="w-8 h-8"></Category>
                 </BottomMenuCardWraper>
-                <BottomMenuCardWraper text="ثبت آگهی" >
-                    <div className="w-12 h-12 bg-slate-600"></div>
+                <BottomMenuCardWraper text='آگهی‌ها' >
+                    <Ads className="w-8 h-8 "></Ads>
                 </BottomMenuCardWraper>
-                <BottomMenuCardWraper text="ثبت آگهی" >
-                    <div className="w-12 h-12 bg-slate-600"></div>
+                <BottomMenuCardWraper text='پیام‌ها' >
+                    <Chat className="w-8 h-8"></Chat>
                 </BottomMenuCardWraper>
-                <BottomMenuCardWraper text="ثبت آگهی" >
-                    <div className="w-12 h-12 bg-slate-600"></div>
+                <BottomMenuCardWraper text='حساب کاربری' >
+                    <Account className="w-8 h-8">
+                    </Account>
                 </BottomMenuCardWraper>
             </div>
 
--- src/components/agahiComponents/bottomMenuCardWraper/index.tsx
+++ src/components/agahiComponents/bottomMenuCardWraper/index.tsx
@@ -2,9 +2,9 @@ import React, { ReactNode } from 'react'
 
 function BottomMenuCardWraper({children, text} : {children: ReactNode, text: string}) {
   return (
-    <div className="h-full w-[20%] flex flex-wrap justify-center items-center">
+    <div className="h-full w-[20%] flex flex-col gap-2 lg:flex-row lg:gap-4 justify-center items-center">
       {children}
-      <div className='text-xs md:text-sm px-2'>
+      <div className='text-xs md:text-sm'>
         {text}
       </div>
 
