79209a0716c5cc458f008fc820595d289c760f18
mohammad <M@>
2024-01-03 15:12:22 +0330

    mobpic

--- src/app/agahi/[city]/[ad]/page.tsx
+++ src/app/agahi/[city]/[ad]/page.tsx
@@ -1,14 +1,16 @@
 import FullScreenMain from '@/components/wraper/fullScreenMain'
 import React from 'react'
 import adpic from '../../../../../public/pic/galaxy.webp'
-import PicSldierShow from '@/components/agahiComponents/picSliderShow'
+import adpic2 from '../../../../../public/pic/galaxy2.jpg'
+import adpic3 from '../../../../../public/pic/galaxy3.png'
+import adpic4 from '../../../../../public/pic/galaxy4.jpg'
 import SwiperSlider from '@/components/agahiComponents/swiperSlider'
 
 function AdPage() {
   return (
     <FullScreenMain>
       <div className='w-full h-[40vh] lg:h-[70vh] mt-11'>
-        <SwiperSlider pics={[adpic ,adpic, adpic, adpic]} />
+        <SwiperSlider pics={[adpic ,adpic2, adpic3, adpic4]} />
       </div>
       <div className='w-full h-[40vh] lg:h-[30vh'>
 
--- src/components/adCard/index.tsx
+++ src/components/adCard/index.tsx
@@ -1,11 +1,15 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import pic from '../../../public/pic/galaxy2.jpg'
 
 function AdCard() {
   return (
     <Link href={'/agahi/mashhad/15'}>
         <div className='h-fit w-full lg:w-fit flex text-sm justify-start gap-10 py-4 px-4 rounded-xl bg-gray-100 dark:bg-stone-900 hover:scale-105'>
-        <div className='bg-gray-200 dark:bg-stone-600 h-[6rem] lg:h-[8rem] w-[6rem] lg:w-[8rem]'></div>
+        <div className='bg-gray-200 dark:bg-stone-600 h-[6rem] lg:h-[8rem] w-[6rem] lg:w-[8rem] relative'>
+            <Image src={pic} alt='' fill/>
+        </div>
         <div className=' flex flex-col justify-between grow'>
             <div className='h-[2rem] w-full text-right'>
                 گوشی سامسونگ مدل اس22 الترا 
