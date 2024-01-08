2d7b4127725d99dfc2dd087cfe11065c990b4f60
mohammad <M@>
2024-01-08 17:34:04 +0330

    bettermessenger

--- src/app/agahi/messenger/page.tsx
+++ src/app/agahi/messenger/page.tsx
@@ -17,23 +17,23 @@ function Messenger() {
           {message1.map(i=>{
       return(
       <div className='w-[99%] py-2 px-1 pe-2 text-center rounded-md bg-gray-50 dark:bg-black flex justify-between items-center' key={Math.random()*100000}>
-      <div className='w-[5rem] h-[5rem] rounded-full bg-gray-200 dark:bg-stone-900'>
+      <div className='w-[4rem] lg:w-[5rem] h-[4rem] lg:h-[5rem] rounded-full bg-gray-200 dark:bg-stone-900'>
 
       </div>
       <div className='h-full py-2 flex flex-col justify-between grow px-2'>
         <div className='w-fit text-md ps-1'>
           {i.contact}
         </div>
-        <div className='ms-4 text-right text-xs h-4 overflow-hidden mb-1'>
+        <div className='ms-6 text-right text-xs h-4 overflow-hidden mb-1 text-gray-500'>
           {i.messages[0].message}
         </div>
       </div>
       <div>
       <div className='flex flex-col gap-2 items-center'>
-            <div className='text-md'>
+            <div className='text-sm text-gray-400'>
               {i.messages[0].date}
             </div>
-            <div className='rounded-full bg-green-400 px-2 pt-1 text-white w-fit'>
+            <div className='rounded-full text-sm bg-green-400 px-2 pt-[2px] text-white w-fit'>
               2
             </div>
             
