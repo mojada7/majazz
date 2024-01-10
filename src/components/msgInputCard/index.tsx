dd90080e62bdbe38000066636b8a91e2949db10f
mohammad <M@>
2024-01-10 16:59:07 +0330

    payam

--- src/components/msgInputCard/index.tsx
+++ src/components/msgInputCard/index.tsx
@@ -6,11 +6,11 @@ function MsgInputCard() {
   return (
     <>
       <div className='h-[80%] rounded-xl w-fit'>
-        <FaceSmile  className='text-sky-300 dark:text-sky-300 h-12 w-12'/> 
+        <FaceSmile  className='text-sky-300 dark:text-sky-300 w-10 lg:h-12 wh10 lg:w-12'/> 
       </div>
-      <input className='h-[80%]  rounded-xl px-4 grow'/>
+      <input dir='rtl' placeholder='پیام ...' className='h-[80%] rounded-md outline-none px-4 grow'/>
       <div className='h-[80%] rounded-xl w-fit'>
-        <SendIcon className='text-sky-300 dark:text-sky-300 h-12 w-12' /> 
+        <SendIcon className='text-sky-300 dark:text-sky-300 w-10 lg:h-12 h-10 lg:w-12' /> 
       </div>
     </>
   )
